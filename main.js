72b9a658105973970fbde0e9150b6e21beb36b84
FaizanKhan7 <mohamedfaizankhan1@gmail.com>
2020-07-17 22:00:56 +0530

    added functionality - remove a book to UI

--- main.js
+++ main.js
@@ -40,11 +40,17 @@ class UI {
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         </td>
-        <td><i class="fas fa-trash-alt" class="btn-remove delete"></i>
+        <td><i class="fas fa-trash-alt btn-remove delete"></i>
         `;
         // we have our list, which we grabed from the DOM and created the new row which has all the stuff in it. so, now we just need to append the row to the list
 
         list.appendChild(row);
+    }
+
+    static deleteBook(element) {
+        if (element.classList.contains('delete')) {
+            element.parentElement.parentElement.remove();
+        }
     }
 
     static clearFields() {
@@ -69,23 +75,29 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 /*Since we have addBookToList method in the UI, so we can utilize the method again, not just with display books but also when we add a book when we hit submit. so to do that we need to handle collecting the data from the form, we need to instantiating a new book and then calling the add to list method . */
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-        // prevet actual submit
-        e.preventDefault();
-        // Get form value
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').value;
-        const isbn = document.querySelector('#isbn').value;
-        // once we get these values we want to instantiate a book from the Book Class, since it is not static we need to instantiate a book too add a book.
+    // prevet actual submit
+    e.preventDefault();
+    // Get form value
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    // once we get these values we want to instantiate a book from the Book Class, since it is not static we need to instantiate a book too add a book.
 
-        // Instantiate book
-        const book = new Book(title, author, isbn);
-        // console.log(book);
-        // we get a book object
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+    // console.log(book);
+    // we get a book object
 
-        // Add Book to UI
-        UI.addBookToList(book);
+    // Add Book to UI
+    UI.addBookToList(book);
 
-        // Clear Feilds
-        UI.clearFields()
-    })
-    // Event: Remove a Book
+    // Clear Feilds
+    UI.clearFields();
+});
+
+
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // console.log(e.target);
+    UI.deleteBook(e.target);
+});
